9be703bfe3a22f79b7192a004b47d915ce46ab7d
kakott <sakaabc150827@gmail.com>
2026-05-29 14:10:33 +0900

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,12 @@
 def fizzbuzz(num):
-"""
-引数が3の倍数なら'Fizz'、5の倍数なら'Buzz'、両方の倍数なら
-'FizzBuzz'を返す。
- いずれの倍数でもない場合は受け取った数値をそのまま返す。
- """
-if num % 3 == 0 and num % 5 == 0:
-   return 'FizzBuzz'
-if num % 3 == 0:
-   return 'Fizz'
-if num % 5 == 0:
-   return 'Buzz'
-return num
+    """
+    引数が3の倍数なら'Fizz'、5の倍数なら'Buzz'、両方の倍数なら'FizzBuzz'を返す。
+    いずれの倍数でもない場合は受け取った数値を文字列として返す。
+    """
+    if num % 3 == 0 and num % 5 == 0:
+        return 'FizzBuzz'
+    if num % 3 == 0:
+        return 'Fizz'
+    if num % 5 == 0:
+        return 'Buzz'
+    return str(num)  # 文字列で返すように修正
