4cf6d6646f070303f47d6f4c899375a648f59837
kakott <sakaabc150827@gmail.com>
2026-05-29 14:10:59 +0900

    Update test_fizzbuzz.py

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,9 +1,15 @@
 import unittest
 import fizzbuzz
+
 class TestFizzBuzz(unittest.TestCase):
-def test_fizz(self):
-    self.assertEqual('Fizz', fizzbuzz.fizzbuzz(3))
-def test_buzz(self):
-    self.assertEqual('Buzz', fizzbuzz.fizzbuzz(5))
-def test_fizzbuzz(self):
-    self.assertEqual('FizzBuzz', fizzbuzz.fizzbuzz(15))
+    def test_fizz(self):
+        self.assertEqual('Fizz', fizzbuzz.fizzbuzz(3))
+
+    def test_buzz(self):
+        self.assertEqual('Buzz', fizzbuzz.fizzbuzz(5))
+
+    def test_fizzbuzz(self):
+        self.assertEqual('FizzBuzz', fizzbuzz.fizzbuzz(15))
+
+if __name__ == '__main__':
+    unittest.main()
