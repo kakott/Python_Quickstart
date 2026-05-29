9f39941c642d6d8aaa54ec0d6160784387a55916
kakott <sakaabc150827@gmail.com>
2026-05-29 14:07:34 +0900

    Update test_fizzbuzz.py

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -2,8 +2,8 @@ import unittest
 import fizzbuzz
 class TestFizzBuzz(unittest.TestCase):
 def test_fizz(self):
-self.assertEqual('Fizz', fizzbuzz.fizzbuzz(3))
+    self.assertEqual('Fizz', fizzbuzz.fizzbuzz(3))
 def test_buzz(self):
-self.assertEqual('Buzz', fizzbuzz.fizzbuzz(5))
+    self.assertEqual('Buzz', fizzbuzz.fizzbuzz(5))
 def test_fizzbuzz(self):
-self.assertEqual('FizzBuzz', fizzbuzz.fizzbuzz(15))
+    self.assertEqual('FizzBuzz', fizzbuzz.fizzbuzz(15))
